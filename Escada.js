aef8a45b9c00d2a95805e0749f370cd86e897074
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-01 17:18:22 -0300

    Create Escada.js

---
+++ Escada.js
@@ -0,0 +1,10 @@
+function escada(altura){
+    let lista = [];
+    for(let i = 1; i <= altura; i++){
+      let espaco = altura - i;
+      lista.push(" ".repeat(espaco) + "#".repeat(i));
+    }
+    return lista;
+  }
+  console.log(escada(5));
+  
