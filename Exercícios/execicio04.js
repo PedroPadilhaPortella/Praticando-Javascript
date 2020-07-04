323f36afc090093b41671cb854c69c34c4da5691
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-04 19:18:54 -0300

    Update execicio04.js

---
+++ Exercícios/execicio04.js
@@ -0,0 +1,10 @@
+function Divisao (dividendo = 0, divisor = 0){
+    return `${dividendo} dividido por ${divisor} = ${dividendo/divisor}`;
+}
+
+console.log(Divisao(15, 3))
+console.log(Divisao(169, 13))
+console.log(Divisao(15, 200))
+console.log(Divisao(13, 0))
+console.log(Divisao(0, 0))
+console.log(Divisao(0, 13))
