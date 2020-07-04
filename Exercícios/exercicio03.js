b1bf731c43725c912ed8d289d6bec970546131d8
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-04 19:13:25 -0300

    Update exercicio03.js

---
+++ Exercícios/exercicio03.js
@@ -0,0 +1,9 @@
+function Exponeciacao (base, expoente){
+    return `${base} elevado à ${expoente} = ${Math.pow(base, expoente)}`;
+}
+
+console.log(Exponeciacao(1, 0))
+console.log(Exponeciacao(5, 2))
+console.log(Exponeciacao(3, 3))
+console.log(Exponeciacao(2, 10))
+console.log(Exponeciacao(9, 4))
