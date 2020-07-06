15c69f7dbdbe04171f193bbc703825138e8e2aa9
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-06 00:41:39 -0300

    Update exercicio12.js

--- Exercícios/exercicio12.js
+++ Exercícios/exercicio12.js
@@ -1,15 +1,15 @@
 /*Faça um algoritmo que calcule o fatorial de um número*/
 
 function fatorial(valor){
-    let fatorial
-    while(valor >= 0){
-        fatorial += valor
+    if(valor == 1){
+        return valor
+    }else{
+        return valor * fatorial(valor - 1)
     }
-    return fatorial
 }
 
 console.log(fatorial(5))
-console.log(fatorial(100))
+console.log(fatorial(7))
 console.log(fatorial(9))
 console.log(fatorial(4))
 console.log(fatorial(3))
