443dd1de03a45eb0b2059bd9ed586677fa1a91c3
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-06 00:26:53 -0300

    Update exercicio11.js

--- Exercícios/exercicio11.js
+++ Exercícios/exercicio11.js
@@ -5,3 +5,28 @@ De 400 em 400 anos é ano bissexto;
 Prevalecem as últimas regras sobre as primeiras.
 Partindo daí elabore uma função que recebe um ano e calcula se ele é ano bissexto, imprimindo no console a
 mensagem e retornando true ou false*/
+
+function anoBissexto(ano){
+    if(ano <= 0){
+        return false
+    }else if(ano % 400 == 0){
+        return true
+    }else if(ano % 100 == 0){
+        return false
+    }else if(ano % 4 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(anoBissexto(0))
+console.log(anoBissexto(4))
+console.log(anoBissexto(100))
+console.log(anoBissexto(400))
+console.log(anoBissexto(800))
+console.log(anoBissexto(2020))
+console.log(anoBissexto(2021))
+console.log(anoBissexto(1960))
+console.log(anoBissexto(1945))
+console.log(anoBissexto(1790))
+console.log(anoBissexto(1346))
