e63cf7922048955a310e16a8447a54d80a7fe7f6
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-07 00:05:07 -0300

    Update exercicio14.js

--- Exercícios/exercicio14.js
+++ Exercícios/exercicio14.js
@@ -2,3 +2,25 @@
 casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com
 escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie
 também um default, que retornará uma mensagem de erro no console*/
+
+function Quitanda(fruta){
+    switch (fruta) {
+        case 'maçã':
+            return "Não vendemos essa fruta aqui"
+            break;
+        case 'kiwi':
+            return "Estamos com escassez de kiwis"
+            break;
+        case 'melancia':
+            return "Aqui está, são 3 reais o quilo"
+        default:
+            return "Não temos esse produto aqui"
+            break;
+    }
+}
+
+console.log(Quitanda('maçã'))
+console.log(Quitanda('kiwi'))
+console.log(Quitanda('melancia'))
+console.log(Quitanda('banana'))
+console.log(Quitanda('melão'))
