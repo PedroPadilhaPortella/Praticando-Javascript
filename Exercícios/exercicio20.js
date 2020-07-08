0c1aca604d33b964d57ae770eb87605f901b9428
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-08 10:31:53 -0300

    Update exercicio20.js

--- Exercícios/exercicio20.js
+++ Exercícios/exercicio20.js
@@ -50,7 +50,7 @@ function sacarDinheiro(valorSaque){
     return concatenarResultados(contador100, contador50, contador20, contador10, contador5, contador2, contador1)
 }
 
-calcularValorNota(valorSaque){
+function calcularValorNotas(valorSaque){
     if(valorSaque >= 100){
         return 100
     }else if(valorSaque >= 50){
@@ -69,5 +69,32 @@ calcularValorNota(valorSaque){
 }
 
 function concatenarResultados(contador100, contador50, contador20, contador10, contador5, contador2, contador1){
-    
+    let resultado = 'Saque de.: '
+
+    if(contador100 > 0){
+        resultado += ` ${contador100} nota(s) de R$ 100.`
+    }
+    if(contador50 > 0){
+        resultado += ` ${contador50} nota(s) de R$ 50.`
+    }
+    if(contador20 > 0){
+        resultado += ` ${contador20} nota(s) de R$ 20.`
+    }
+    if(contador10 > 0){
+        resultado += ` ${contador10} nota(s) de R$ 10.`
+    }
+    if(contador5 > 0){
+        resultado += ` ${contador5} nota(s) de R$ 5.`
+    }
+    if(contador2 > 0){
+        resultado += ` ${contador2} nota(s) de R$ 2.`
+    }
+    if(contador1 > 0){
+        resultado += ` ${contador1} nota(s) de R$ 1.`
+    }
+    return resultado
 }
+
+console.log(sacarDinheiro(188))
+console.log(sacarDinheiro(1250))
+console.log(sacarDinheiro(77))
