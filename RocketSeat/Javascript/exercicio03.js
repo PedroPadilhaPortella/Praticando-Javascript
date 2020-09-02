43ad189602b570b40b6015c2a497cf61a93022e8
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-09-02 11:34:56 -0300

    criando fogo do Doom e outras alteracoes

--- RocketSeat/Javascript/exercicio03.js
+++
@@ -1,9 +0,0 @@
-function temHabilidade(skills) {
-    var contain = false;
-    skills.forEach(element => {
-        if(element === 'Javascript') contain =  true;
-    });
-    return contain;
-    }
-var skills = ["Javascript", "ReactJS", "React Native"]
-console.log(temHabilidade(skills)); // true ou false
